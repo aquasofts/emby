e8fb23f8fb250f2eaf8c5587867c69ac0047694e
aquasofts <aquasofts520@gmail.com>
2024-09-07 12:42:20 +0800

    更新了反代前后端分离emby的脚本（实验性）

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 echo "如果输入 '1'，则安装 http 协议；"
 echo "如果输入 '2'，则安装 https 协议(需要提前准备好域名证书)"
 echo "如果输入 '3'，则安装 https 协议(安装过程中将会自动申请证书)"
+echo "如果输入 '4'，则安装 https 协议(emby服务器含有推流地址选这个)(安装过程中将会自动申请证书)"
 read input
 
 if [ "$input" == "1" ]; then
@@ -11,6 +12,8 @@ elif [ "$input" == "2" ]; then
     wget -N --no-check-certificate https://raw.githubusercontent.com/aquasofts/emby/main/sh/emby2.sh && chmod +x emby2.sh && ./emby2.sh
 elif [ "$input" == "3" ]; then
     wget -N --no-check-certificate https://raw.githubusercontent.com/aquasofts/emby/main/sh/emby3.sh && chmod +x emby3.sh && ./emby3.sh
+ elif [ "$input" == "4" ]; then
+    wget -N --no-check-certificate https://raw.githubusercontent.com/aquasofts/emby/main/sh/emby5.sh && chmod +x emby5.sh && ./emby5.sh
 else
     echo "无效输入，请输入 '1' 或 '2' 或 '3'"
 fi
